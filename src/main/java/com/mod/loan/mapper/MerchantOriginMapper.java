92466aacb86ebe9be8182a0a1e940e234a0dddac
duanyi <d5278779@163.com>
2019-06-14 10:46:07 +0800

    短信签名按渠道设置的签名发送

---
+++ src/main/java/com/mod/loan/mapper/MerchantOriginMapper.java
@@ -0,0 +1,7 @@
+package com.mod.loan.mapper;
+
+import com.mod.loan.common.mapper.MyBaseMapper;
+import com.mod.loan.model.MerchantOrigin;
+
+public interface MerchantOriginMapper extends MyBaseMapper<MerchantOrigin> {
+}
