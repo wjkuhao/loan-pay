acb60e24cb4dc54330ec7f9b5868314e06030830
kibear <pengzhineng143@126.com>
2019-06-20 11:53:22 +0800

    商户最大产品额度限制改为配置

--- src/main/java/com/mod/loan/config/Constant.java
+++ src/main/java/com/mod/loan/config/Constant.java
@@ -6,61 +6,74 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Constant {
 
-	public static String ENVIROMENT;
+    public static String ENVIROMENT;
 
-	/**
-	 * oss
-	 * */
-	public static String OSS_STATIC_BUCKET_NAME;
-	public static String OSS_ACCESSKEY_ID;
-	public static String OSS_ACCESS_KEY_SECRET;
-	public static String OSS_ENDPOINT_OUT_URL;
-	public static String OSS_ENDPOINT_IN_URL;
+    /**
+     * oss
+     */
+    public static String OSS_STATIC_BUCKET_NAME;
+    public static String OSS_ACCESSKEY_ID;
+    public static String OSS_ACCESS_KEY_SECRET;
+    public static String OSS_ENDPOINT_OUT_URL;
+    public static String OSS_ENDPOINT_IN_URL;
 
-	/**
-	 * 合利宝委托代付
-	 * */
-	public static String HELIPAY_ENTRUSTED_URL;
-	public static String HELIPAY_ENTRUSTED_FILE_URL;
+    /**
+     * 合利宝委托代付
+     */
+    public static String HELIPAY_ENTRUSTED_URL;
+    public static String HELIPAY_ENTRUSTED_FILE_URL;
 
-	@Value("${environment:}")
-	public void setENVIROMENT(String environment) {
-		Constant.ENVIROMENT = environment;
-	}
+    /**
+     * 商户最大额度限制改成配置
+     */
+    public static Integer MERCHANT_MAX_PRODUCT_MONEY;
 
-	@Value("${oss.static.bucket.name:}")
-	public void setOSS_STATIC_BUCKET_NAME(String oSS_STATIC_BUCKET_NAME) {
-		OSS_STATIC_BUCKET_NAME = oSS_STATIC_BUCKET_NAME;
-	}
+    @Value("${environment:}")
+    public void setENVIROMENT(String environment) {
+        Constant.ENVIROMENT = environment;
+    }
 
-	@Value("${oss.accesskey.id:}")
-	public void setOSS_ACCESSKEY_ID(String oSS_ACCESSKEY_ID) {
-		OSS_ACCESSKEY_ID = oSS_ACCESSKEY_ID;
-	}
+    @Value("${oss.static.bucket.name:}")
+    public void setOSS_STATIC_BUCKET_NAME(String oSS_STATIC_BUCKET_NAME) {
+        OSS_STATIC_BUCKET_NAME = oSS_STATIC_BUCKET_NAME;
+    }
 
-	@Value("${oss.accesskey.secret:}")
-	public void setOSS_ACCESS_KEY_SECRET(String oSS_ACCESS_KEY_SECRET) {
-		OSS_ACCESS_KEY_SECRET = oSS_ACCESS_KEY_SECRET;
-	}
+    @Value("${oss.accesskey.id:}")
+    public void setOSS_ACCESSKEY_ID(String oSS_ACCESSKEY_ID) {
+        OSS_ACCESSKEY_ID = oSS_ACCESSKEY_ID;
+    }
 
-	@Value("${oss.endpoint.out:}")
-	public void setOssEndpointOutUrl(String ossEndpointOutUrl) {
-		Constant.OSS_ENDPOINT_OUT_URL = ossEndpointOutUrl;
-	}
+    @Value("${oss.accesskey.secret:}")
+    public void setOSS_ACCESS_KEY_SECRET(String oSS_ACCESS_KEY_SECRET) {
+        OSS_ACCESS_KEY_SECRET = oSS_ACCESS_KEY_SECRET;
+    }
 
-	@Value("${oss.endpoint.in:}")
-	public void setOssEndpointInUrl(String ossEndpointInUrl) {
-		Constant.OSS_ENDPOINT_IN_URL = ossEndpointInUrl;
-	}
+    @Value("${oss.endpoint.out:}")
+    public void setOssEndpointOutUrl(String ossEndpointOutUrl) {
+        Constant.OSS_ENDPOINT_OUT_URL = ossEndpointOutUrl;
+    }
 
-	@Value("${helipay.entrusted.url:}")
-	public void setHelipayEntrustedUrl(String helipayEntrustedUrl) {
-		Constant.HELIPAY_ENTRUSTED_URL = helipayEntrustedUrl;
-	}
+    @Value("${oss.endpoint.in:}")
+    public void setOssEndpointInUrl(String ossEndpointInUrl) {
+        Constant.OSS_ENDPOINT_IN_URL = ossEndpointInUrl;
+    }
 
-	@Value("${helipay.entrusted.file.url:}")
-	public void setHelipayEntrustedFileUrl(String helipayEntrustedFileUrl) {
-		Constant.HELIPAY_ENTRUSTED_FILE_URL = helipayEntrustedFileUrl;
-	}
+    @Value("${helipay.entrusted.url:}")
+    public void setHelipayEntrustedUrl(String helipayEntrustedUrl) {
+        Constant.HELIPAY_ENTRUSTED_URL = helipayEntrustedUrl;
+    }
+
+    @Value("${helipay.entrusted.file.url:}")
+    public void setHelipayEntrustedFileUrl(String helipayEntrustedFileUrl) {
+        Constant.HELIPAY_ENTRUSTED_FILE_URL = helipayEntrustedFileUrl;
+    }
+
+    @Value("${merchant.max.product.money:5000}")
+    public void setMerchantMaxProductMoney(Integer merchantMaxProductMoney) {
+        if (merchantMaxProductMoney > 10000) {
+            merchantMaxProductMoney = 10000;
+        }
+        MERCHANT_MAX_PRODUCT_MONEY = merchantMaxProductMoney;
+    }
 
 }
